45bfdf428fa2b9a75500d1baa3c992cf45f53756
Juan M Z <juanmaznn@gmail.com>
2024-06-22 11:11:35 -0300

    config db, create models category and product

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'date_register')
+    
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name_product', 'price')
+    list_editable = ('price',)
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
--- web/models.py
+++ web/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='nombre')
+    date_register = models.DateTimeField(auto_now_add=True, verbose_name='fecha de registro')
+
+    class Main:
+        pass
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.RESTRICT, verbose_name='categoria')
+    name_product = models.CharField(max_length=200, verbose_name='nombre producto')
+    description = models.TextField(null=True, verbose_name='descripcion')
+    price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='precio')
+    date_register = models.DateTimeField(auto_now_add=True, verbose_name='fecha de registro')
+    imagen = models.ImageField(upload_to='product', blank=True)
+
+    def __str__(self):
+        return self.name_product
